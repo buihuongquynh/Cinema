ec757f8d2566e2b277cc1fab9b005806745e01b2
buihuongquynh <buigiangquynh@gmail.com>
2022-04-11 22:26:12 +0700

    fix

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -5,27 +5,15 @@ import './css.css';
 const Page404 = () => {
   return (
     <div id="notfound">
-        <div className="notfound-bg">
-          <div />
-          <div />
-          <div />
-          <div />
-        </div>
-        <div className="notfound">
-          <div className="notfound-404">
-            <h1>404</h1>
-          </div>
-          <h2>Page Not Found</h2>
-          <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-          <a href="/">Homepage</a>
-          <div className="notfound-social">
-            <a href="#"><i className="fa fa-facebook" /></a>
-            <a href="#"><i className="fa fa-twitter" /></a>
-            <a href="#"><i className="fa fa-pinterest" /></a>
-            <a href="#"><i className="fa fa-google-plus" /></a>
-          </div>
-        </div>
+    <div className="notfound">
+      <div className="notfound-404">
+        <h1>4<span />4</h1>
       </div>
+      <h2>Oops! Page Not Be Found</h2>
+      <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+      <a href="/">Back to homepage</a>
+    </div>
+  </div>
   );
 };
 
